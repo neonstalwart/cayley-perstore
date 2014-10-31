4ee360fe4e076d8ad02ad5b63ee16993aa11182d
ben hockey <neonstalwart@gmail.com>
2014-10-31 08:50:38 -0500

    update dependencies

--- tests/index.js
+++ tests/index.js
@@ -100,6 +100,12 @@ define(function (require) {
 			}
 		},
 
+		setSchema: {
+			'throws if schema has already been set': function () {
+				this.skip('implement setSchema throws when already set');
+			}
+		},
+
 		put: {
 			'returns a promise': function () {
 				var result = store.put(value);
@@ -119,6 +125,91 @@ define(function (require) {
 				.then(function (actual) {
 					assert.deepEqual(actual, value, 'store.put should insert values into the db');
 				});
+			},
+
+			'resolves to identifier of object': function () {
+				return this.skip('not implemented');
+				var value = {
+						name: 'bar'
+					};
+
+				return store.put(value).then(function (key) {
+					return Q.ninvoke(db, 'get', key)
+						.then(function (actual) {
+							assert.strictEqual(actual.id,  key, 'key should be generated when not provided');
+							assert.strictEqual(actual.name, value.name, 'store.put should resolve to the key');
+						});
+				});
+			},
+
+			options: {
+				'overwrite: false fails if value already exists for key': function () {
+					return this.skip('not implemented');
+					var options = {
+							overwrite: false
+						},
+						key = 'alice',
+						value = {
+							id: key,
+							name: 'bar'
+						};
+
+					return store.put(value).then(function () {
+						return store.put(value, options);
+					})
+					.then(function () {
+						assert.fail('overwrite: false should not overwrite an existing value');
+					})
+					.catch(function (err) {
+						assert.fail('cayley-perstore: tried to overwrite existing object "' + value.id + '"');
+						assert.propertyVal(err, 'code', 'EXISTS');
+					});
+				},
+
+				'can add values in a tight loop': function () {
+					return this.skip('not implemented');
+					var keys = [],
+						numValues = 2000,
+						options = {
+							overwrite: false
+						};
+
+					while (numValues--) {
+						keys.push(Math.random());
+					}
+
+					return Q.all(keys.map(function (key) {
+							return store.put({
+								id: key
+							}, options);
+						}))
+						.then(function (actual) {
+							assert.deepEqual(actual, keys, 'parallel adds should be possible');
+						});
+				},
+
+				'id indicates key for object': function () {
+					return this.skip('not implemented');
+					var key = 'foo',
+						options = {
+							id: key
+						},
+						value = {
+							id: 'baz',
+							name: 'bar'
+						},
+						expected = {
+							id: key,
+							name: 'bar'
+						};
+
+					return store.put(value, options).then(function () {
+						return Q.ninvoke(db, 'get', key);
+					})
+					.then(function (actual) {
+						assert.deepEqual(actual, expected, 'options.id should specify id of value');
+					});
+				}
 			}
 		},
 
@@ -158,13 +249,110 @@ define(function (require) {
 
 				return store.put(value)
 					.then(function (id) {
-						console.log(id);
 						return store.get(key);
 					})
 					.then(function (actual) {
 						assert.deepEqual(actual, value, 'store.get should retrieve values stored at key');
 					});
+			},
+
+			'should always return new objects': function () {
+				return this.skip('not implemented');
+				var key = 'foo',
+					value = {
+						id: key,
+						name: 'bar'
+					};
+
+				return store.put(value)
+					.then(function () {
+						return Q.all([
+							store.get(key),
+							store.get(key)
+						]);
+					})
+					.then(function (gets) {
+						gets.reduce(function (one, another) {
+							assert.notEqual(one, another, 'store.get should return new instances');
+							return another;
+						}, value);
+					});
 			}
+		},
+
+		delete: {
+			'should remove an object from the db': function () {
+				return this.skip('not implemented');
+				var key = 'foo',
+					value = {
+						id: key,
+						name: 'bar'
+					};
+
+				return store.put(value)
+					.then(function () {
+						return store.delete(key);
+					})
+					.then(function () {
+						return store.get(key);
+					})
+					.then(function (obj) {
+						assert.isUndefined(obj, 'delete should remove objects from the db');
+					});
+			}
+		},
+
+		query: {
+			'returns a promise for a forEachable stream': function () {
+				return this.skip('not implemented');
+				var key = data[0].id,
+					query = new Query().eq('id', key),
+					results = store.query(query);
+
+				assert.isFunction(results.then, 'query results should be a promise');
+
+				return results.then(function (stream) {
+					assert.isFunction(stream.forEach, 'results should resolve to a forEachable');
+					return stream.forEach(function (item) {
+						assert.propertyVal(item, 'id', key, 'items should match query');
+					});
+				});
+			},
+
+			'limits results based on query': function () {
+				return this.skip('not implemented');
+				var query = new Query().gt('id', 0).limit(2);
+
+				return store.query(query).then(function (stream) {
+					var length = 0;
+					return stream.forEach(function (item) {
+						assert.ok(item.id > 0, 'items should match query');
+						length++;
+					})
+					.then(function () {
+						assert.strictEqual(length, 2, 'query limit should be applied');
+					});
+				});
+			},
+
+			'includes a forEach convenience method': function () {
+				return this.skip('not implemented');
+				var key = data[0].id,
+					query = new Query().eq('id', key),
+					results = store.query(query),
+					count = 0;
+
+				assert.isFunction(results.forEach, 'results should have a forEach convenience method');
+				return results.forEach(function (item) {
+					assert.propertyVal(item, 'id', key, 'items should match query');
+					count++;
+				})
+				.then(function () {
+					assert.strictEqual(count, 1);
+				});
+			}
+
+			// TODO: sort(?!)
 		}
 	});
 });
